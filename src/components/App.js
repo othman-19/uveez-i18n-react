edae4b9aceef9976751b5035eb7e0b717e1d2114
othmane <othman-19@hotmail.fr>
2021-01-16 18:22:52 +0100

    implementing routing and creating our basic app layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import routes from '../routes';
+import AppNavbar from '../components/AppNavbar';
+import AppFooter from '../components/AppFooter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div style={{ paddingTop: '80px' }}>
+    <Router>
+      <div>
+        <AppNavbar />
+        <div className="container">
+          <main id="main" role="main">
+            <Switch>
+              {routes.map((route, index) => (
+                <Route
+                  // eslint-disable-next-line react/no-array-index-key
+                  key={index}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.component}
+                />
+              ))}
+            </Switch>
+          </main>
+        </div>
+        <AppFooter />
+      </div>
+    </Router>
+  </div>
+);
 
 export default App;
