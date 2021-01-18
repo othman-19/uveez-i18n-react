ead3727e45e70ff761d159bfe575eaf8dc5b4a2c
othmane <othman-19@hotmail.fr>
2021-01-18 21:23:20 +0100

    rewright the defaultOnUndefinedOrNull in ES6 syntax

--- src/services/util.js
+++ src/services/util.js
@@ -1,7 +1,8 @@
-export function defaultOnUndefinedOrNull (value, _default) {
+const defaultOnUndefinedOrNull = (value, _default) => {
   if (value === undefined || value === null) {
-      return _default
+    return _default;
   }
+  return value;
+};
 
-  return value
-}
+export default defaultOnUndefinedOrNull;
