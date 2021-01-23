124a4d1ae5b94c841ed2d2345b77da7a1cf046b2
othmane <othman-19@hotmail.fr>
2021-01-23 21:48:47 +0100

    Implement the setUiLocale function

---
+++ front/src/containers/Localizer.js
@@ -0,0 +1,6 @@
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { locales } from '../config/i18n';
+import { setUiLocale } from '../services/i18n';
+
---
+++ front/src/services/i18n/index.js
@@ -0,0 +1,34 @@
+import i18next from 'i18next';
+
+import { formatDate } from './util';
+
+export const setUiLocale = locale => fetch(`/translations/${locale}.json`)
+  .then(response => response.json())
+  .then(loadedResources => (
+    new Promise((resolve, reject) => {
+      i18next.init({
+        lng: locale,
+        debug: true,
+        resources: { [locale]: loadedResources },
+        interpolation: {
+          format(value, format, locale) {
+            if (value instanceof Date) {
+              return formatDate(value, format, locale);
+            }
+
+            return value;
+          },
+        },
+      }, (err, t) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        resolve();
+      });
+    })
+  ))
+  .catch(err => Promise.reject(err));
+
+export const t = (key, opt) => i18next.t(key, opt);
